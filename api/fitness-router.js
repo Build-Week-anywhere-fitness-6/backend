b615185e002d064719549700d855ff11527075ef
Charles Qian <charleshqian@gmail.com>
2021-10-21 16:35:37 -0700

    started router

--- api/fitness-router.js
+++ api/fitness-router.js
@@ -1,7 +1,14 @@
+const { validatePassword, validateUsername } = require("./fitness-middleware");
+
 const router = require("express").Router();
 
-router.post('/login', (req, res) => {
-    res.send('this is a login page') 
+router.post('/login', validatePassword, validateUsername, (req, res) => {
+    if (bcrypt.compareSync(req.body.password, req.user.password)) {
+        const token = buildToken(req.user)
+        res.status(200).json({message:`${req.user.username} is back!`, token})
+    } else {
+        next({status:401, message:'Invalid credentials'})
+    } 
 })
 
 
@@ -13,5 +20,20 @@ router.get('/logout', (req, res) => {
     res.send('this is a logout page') 
 })
 
+function buildToken (user) {
+    const payload = {
+      username: user.username,
+      role: user.role
+    }
+    const options = {
+      expiresIn: '1d'
+    }
+    const token = jwt.sign(
+      payload,
+      JWT_SECRET,
+      options,
+    )
+    return token
+  }
 
 module.exports = router;
