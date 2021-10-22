2bf003e59c80ebcc453230c0741363e1bc000132
courtneyharris <harriscourtney98@gmail.com>
2021-10-22 17:51:21 -0400

    Routers built out

--- api/server.js
+++ api/server.js
@@ -6,6 +6,28 @@ const server = express();
 server.use(express.json());
 server.use('/api/auth', authRouter)
 
+server.use(session({
+  name: 'chocolatechip',
+  secret: 'no no no', 
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    
+    secure: false,
+    httpOnly: false, 
+  },
+  rolling: true,
+  resave: false, 
+  saveUninitialized: false, 
+  store: new Store({
+    knex: require('../data/dbConfig'),
+    tablename: 'sessions',
+    sidfieldname: 'foo',
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  })
+}))
+
+
 server.use((err, req, res, next) => {
     res.status(err.status || 500).json({
       message: err.message,
